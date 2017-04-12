5750b3c5f3cce79cfd364521b1bdac5968492f3e
kazuhumikobayashi <k.kobayashi@technoplan-inc.co.jp>
2017-04-12 11:12:55 +0900

    文字列を囲む"を'に修正。 refs_#70 他のテーブルでもダブルクォーテーションになっていたinsert文が有ったので、全て修正しました。

--- data/sql/03-data-statuses.sql
+++ data/sql/03-data-statuses.sql
@@ -1,5 +1,5 @@
 insert into statuses (id, status_name, created_at, created_user, updated_at, updated_user) values
-  (1, "見積り中", CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  (2, "受注済", CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  (3, "完了", CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  (4, "失注", CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴");
+  (1, '見積り中', CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  (2, '受注済', CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  (3, '完了', CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  (4, '失注', CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴');
--- data/sql/05-data-departments.sql
+++ data/sql/05-data-departments.sql
@@ -1,8 +1,8 @@
 insert into departments (department_name, created_at, created_user, updated_at, updated_user) values
-  ("第１システム部",  CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  ("第２システム部",  CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  ("第３システム部",  CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  ("第４システム部",  CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  ("総務部",  CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  ("営業部",  CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴"),
-  ("仙台営業所",  CURRENT_TIMESTAMP, "竹内大貴", CURRENT_TIMESTAMP, "竹内大貴");
+  ('第１システム部',  CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  ('第２システム部',  CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  ('第３システム部',  CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  ('第４システム部',  CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  ('総務部',  CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  ('営業部',  CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴'),
+  ('仙台営業所',  CURRENT_TIMESTAMP, '竹内大貴', CURRENT_TIMESTAMP, '竹内大貴');
