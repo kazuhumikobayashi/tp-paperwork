6f58d53da28d2c324d78097c3b83be6a24373bf8
dtakeuchi-tp <d.takeuchi@technoplan-inc.co.jp>
2017-06-15 13:44:56 +0900

    祝日検索画面で今年を取得するタイミングが画面インスタンス生成前になっているので、画面インスタンス生成時に変更する。 #250

--- application/controllers/form/holiday_search_form.py
+++ application/controllers/form/holiday_search_form.py
@@ -3,15 +3,15 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import validators, SelectField
 
-year = datetime.today().year
-last_year = str(year-1)
-this_year = str(year)
-next_year = str(year+1)
-
 
 class HolidaySearchForm(FlaskForm):
+    _year = datetime.today().year
+    _last_year = str(_year - 1)
+    _this_year = str(_year)
+    _next_year = str(_year + 1)
+
     year = SelectField('年', [validators.optional()],
-                       choices=[("", ""), (last_year, last_year + "年"),
-                                (this_year, this_year + "年"), (next_year, next_year + "年")],
-                       default=this_year,
+                       choices=[("", ""), (_last_year, _last_year + "年"),
+                                (_this_year, _this_year + "年"), (_next_year, _next_year + "年")],
+                       default=_this_year,
                        render_kw={"data-placeholder": "年", "data-minimum-results-for-search": "Infinity"})
